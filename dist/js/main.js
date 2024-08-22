64f5c32225900785a78a044b8cf01a95d17a59ea
Sushil Verma <115399549+Sunyvrm1@users.noreply.github.com>
2024-08-22 19:59:43 +0530

    day1

---
+++ dist/js/main.js
@@ -0,0 +1,73 @@
+// 2nd slider content
+
+fetch("slider.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    data.anime.map((suny, index) => {
+      const v1 = 10 - index;
+      const zero = v1 < 10 ? `0${v1}` : `${v1}`;
+      const SliderTrend = document.querySelector(".sliderOuterCont");
+      SliderTrend.insertAdjacentHTML(
+        "afterbegin",
+        ` <div class="slideTrend">
+          <div class="trentTitle">
+            <p class="trendCount">${zero}</p>
+            <p class="trendName trendNameDots">${suny.title_english}</p>
+          </div>
+          <div class="trentImage">
+            <img src="${suny.images}" alt="${suny.anime_id}" class="trentImageImg">
+          </div>
+        </div>`
+      );
+    });
+
+    //slider Button code
+
+    const slide = document.querySelectorAll(".slideTrend");
+    const left = document.querySelector(".sliderleft");
+    const right = document.querySelector(".sliderright");
+    let curSlide = 0;
+    const maxSlide = slide.length;
+
+    const gotoSlide = function (gotoSlide) {
+      slide.forEach((s, i) => {
+        s.style.transform = `translateX(${100 * (i - gotoSlide)}%)`;
+      });
+    };
+
+    gotoSlide(0);
+
+    const next = function () {
+      if (curSlide == maxSlide - 7) {
+      } else {
+        curSlide++;
+      }
+      gotoSlide(curSlide);
+    };
+
+    const prev = function () {
+      if (curSlide == 0) {
+      } else {
+        curSlide--;
+      }
+      gotoSlide(curSlide);
+    };
+
+    right.addEventListener("click", next);
+    left.addEventListener("click", prev);
+
+    //slider next page code
+
+    const trentImageImg = document.querySelectorAll(".trentImageImg");
+    trentImageImg.forEach((img) => {
+      img.addEventListener("click", () => {
+        const getId = img.getAttribute("alt");
+        localStorage.setItem("getId", getId);
+        window.location.href = "anime.html";
+      });
+    });
+  });
+
+
+
